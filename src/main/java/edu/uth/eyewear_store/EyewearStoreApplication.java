68579ff98193e9e169058fb79bd9b97267e837cb
Huy09zhh <huy09zh@gmail.com.com>
2026-04-26 11:52:52 +0700

    Khởi tạo lại dự án hệ thống bán kính

---
+++ src/main/java/edu/uth/eyewear_store/EyewearStoreApplication.java
@@ -0,0 +1,13 @@
+package edu.uth.eyewear_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyewearStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyewearStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/uth/eyewear_store/EyewearStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uth.eyewear_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EyewearStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
